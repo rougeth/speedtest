b302ddece57f33dc3c39bab94874c436721bc924
Marco Rougeth <marco@rougeth.com>
2021-04-11 20:14:11 +0100

    Replace speedtest-cli with oficial Speedtest CLI app

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,17 @@
-FROM python:slim
+FROM ubuntu
+
+ARG INSTALL_KEY=379CE192D401AB61
+ARG DEB_DISTRO=bionic
+
+RUN apt-get update \
+	&& apt-get install -y gnupg1 apt-transport-https dirmngr \
+	&& apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY \
+	&& echo "deb https://ookla.bintray.com/debian ${DEB_DISTRO} main" | tee  /etc/apt/sources.list.d/speedtest.list \
+	&& apt-get update \
+	&& apt-get install -y speedtest python3-dev python3-pip
+
 WORKDIR /usr/src/app
-COPY . .
-RUN pip install -r requirements.txt
-ENTRYPOINT ["python", "speed.py"]
+COPY requirements.txt .
+RUN python3 -m pip install -r requirements.txt
+COPY speed.py .
+ENTRYPOINT ["python3", "speed.py"]
--- speed.py
+++ speed.py
@@ -6,6 +6,7 @@ import time
 from datetime import datetime, timedelta
 from pathlib import Path
 from pprint import pprint
+import tarfile
 
 import click
 import matplotlib.dates as md
@@ -13,29 +14,43 @@ import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 import pandas as pd
 import schedule
+import seaborn as sns
 import telebot
 from loguru import logger
 
 
 ALLOWED_IDS = []
 REPORTS_PATH = ""
+FREQUENCY = int(os.environ.get("FREQUENCY", 10))
 telegram = telebot.TeleBot("invalid-token", threaded=False, skip_pending=True)
+
+
+def dict_item_or_value(item, key, multiplier=1):
+    if isinstance(item, dict):
+        return item[key] * multiplier
+
+    return item
 
 
 def reports(f):
     def wrapper(path, *args, **kwargs):
         logger.info(f"Starting new report. report={f!r}, path={path!r}")
-        reports = sorted(path.iterdir(), reverse=True)
-        reports = filter(f, reports)
+        with tarfile.open(path) as archive:
+            files = archive.getmembers()
 
-        data = []
-        for file in reports:
-            try:
-                data.append(json.load(file.open()))
-            except json.decoder.JSONDecodeError:
-                logger.error(f"Erro while opening file. file={file.name!r}")
-        logger.info(f"Report loaded. total={len(data)}")
-        return data
+            reports = sorted(files, key=lambda f: f.name, reverse=True)
+            reports = filter(lambda f: not "/._" in f.name, reports)
+            reports = filter(f, reports)
+
+            data = []
+            for file in reports:
+                report = archive.extractfile(file.name)
+                try:
+                    data.append(json.load(report))
+                except json.decoder.JSONDecodeError:
+                    logger.exception(f"Erro while opening file. file={file.name!r}")
+            logger.info(f"Report loaded. total={len(data)}")
+            return data
 
     return wrapper
 
@@ -43,26 +58,33 @@ def reports(f):
 @reports
 def report_today(file):
     today = datetime.now().strftime("%Y%m%d")
-    return file.name.startswith(today)
+    return today in file.name
 
 
 @reports
 def report_last_week(file):
+    name = file.name.split("/")[-1]
     last_week = datetime.now() - timedelta(days=7)
     last_week = last_week.strftime("%Y%m%d")
-    return file.name > last_week
+    return name > last_week
 
 
-def dashboard_download_upload_ping(data):
+def dashboard_download_upload_ping(data, date_formatter):
     if not data:
         return False
 
     df = pd.DataFrame(data)
+
+    # Required for migration from speedtest-cli to the SpeedTest CLI by Ookla
+    df["download"] = df["download"].apply(lambda x: dict_item_or_value(x, "bandwidth", 8))
+    df["upload"] = df["upload"].apply(lambda x: dict_item_or_value(x, "bandwidth", 8))
+    df["ping"] = df["ping"].apply(lambda x: dict_item_or_value(x, "latency"))
+
     df["timestamp"] = pd.to_datetime(df["timestamp"])
     df["download"] = df["download"].apply(lambda x: (x / 10 ** 6))
     df["upload"] = df["upload"].apply(lambda x: (x / 10 ** 6))
 
-    plt.grid(color="black", linestyle="-.", linewidth=0.7)
+    sns.set_style("ticks")
     fig = plt.figure(tight_layout=True)
     gs = gridspec.GridSpec(2, 1)
 
@@ -70,61 +92,71 @@ def dashboard_download_upload_ping(data):
         {
             "ylabel": "mbps",
             "plots": [
-                ("timestamp", "download", "Download"),
-                ("timestamp", "upload", "Upload"),
+                ("timestamp", "download", "Download", "#3d5a80"),
+                ("timestamp", "upload", "Upload", "#98c1d9"),
             ],
         },
         {
             "ylabel": "ms",
             "xlabel": "date",
-            "plots": [("timestamp", "ping", "Ping")],
+            "plots": [("timestamp", "ping", "Ping", "#ee6c4d")],
         },
     ]
 
     for i, plot in enumerate(plots):
         ax = fig.add_subplot(gs[i, 0])
-        for x, y, label in plot["plots"]:
-            ax.plot(df[x], df[y], label=label)
+        for x, y, label, color in plot["plots"]:
+            sns.lineplot(data=df, x=x, y=y, ax=ax, color=color)
 
+        sns.despine()
         ax.set_ylabel(plot["ylabel"])
-        ax.xaxis.set_major_formatter(md.DateFormatter("%H:%M"))
+        ax.xaxis.set_major_formatter(md.DateFormatter(date_formatter))
         for tick in ax.get_xticklabels():
             tick.set_rotation(45)
         ax.grid()
         if plot.get("xlabel"):
             ax.set_xlabel("date")
+        else:
+            ax.set_xlabel("")
+
 
     return fig
 
 
 def dashboard_today(reports_path):
     data = report_today(reports_path)
-    return dashboard_download_upload_ping(data)
+    return dashboard_download_upload_ping(data, "%H:%M")
 
 
 def dashboard_last_week(reports_path):
     data = report_last_week(reports_path)
-    return dashboard_download_upload_ping(data)
+    return dashboard_download_upload_ping(data, "%d %b")
 
 
 def job_speed_test(path):
     logger.info("Job started: job_speed_test")
     try:
         output = subprocess.check_output(
-            "speedtest-cli --json", encoding="utf-8", shell=True
+            "speedtest --accept-gdpr --accept-license --format json", encoding="utf-8", shell=True
         )
     except subprocess.CalledProcessError:
         logger.exception("speedtest-cli call failed")
         return
 
     data = json.loads(output)
-    download = data["download"] / 10 ** 6
-    upload = data["upload"] / 10 ** 6
-    logger.info(f"Download={download:.2f}, Upload={upload:.2f}, Ping={data['ping']!r}")
+    download = data["download"]["bandwidth"] * 8 / 10 ** 6
+    upload = data["upload"]["bandwidth"] * 8 / 10 ** 6
+    logger.info(f"Download={download:.2f}Mbps, Upload={upload:.2f}Mbps, Ping={data['ping']!r}")
 
     filename = "{:%Y%m%d-%H%M}.json".format(datetime.now())
-    with (path / filename).open("w") as fp:
-        fp.write(output)
+    with tarfile.open(path, mode="a") as tar:
+        output = output.encode("utf-8")
+        file = io.BytesIO(output)
+        tarinfo = tarfile.TarInfo(name=filename)
+        tarinfo.mtime = time.time()
+        tarinfo.size = len(output)
+
+        tar.addfile(tarinfo, fileobj=file)
     logger.info("Job finished: job_speed_test")
 
 
@@ -199,7 +231,7 @@ def monitor(path):
     logger.info("Starting speedtest app")
     logger.info(f"Output folder: {REPORTS_PATH}")
     logger.info("Starting scheduler")
-    schedule.every(10).minutes.do(job_speed_test, path=REPORTS_PATH).run()
+    schedule.every(FREQUENCY).minutes.do(job_speed_test, path=REPORTS_PATH).run()
     while True:
         schedule.run_pending()
         time.sleep(1)
@@ -208,17 +240,28 @@ def monitor(path):
 @cli.command()
 def test():
     output = subprocess.check_output(
-        "speedtest-cli --json", encoding="utf-8", shell=True
+        "speedtest --accept-gdpr --accept-license --format json", encoding="utf-8", shell=True
     )
     output = json.loads(output)
 
-    download = output["download"] / 10 ** 6
-    upload = output["upload"] / 10 ** 6
-    click.echo(f"Download: {upload:.2f}Mbps")
+    download = output["download"]["bandwidth"] * 8 / 10 ** 6
+    upload = output["upload"]["bandwidth"] * 8 / 10 ** 6
+
+    click.echo(f"Download: {download:.2f}Mbps")
     click.echo(f"Upload: {upload:.2f}Mbps")
     click.echo(f"Ping: {output['ping']}")
     click.echo(f"")
     pprint(output)
+
+
+@cli.command()
+@click.option("--path", required=True, help="Path of speed test reports")
+def report(path):
+    plot = dashboard_last_week(Path(path))
+    plot.savefig("output_last_week.png")
+
+    plot = dashboard_today(Path(path))
+    plot.savefig("output_today.png")
 
 
 if __name__ == "__main__":
